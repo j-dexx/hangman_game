129d3fbc7d5effb441483f28fde2c0f692e61996
James Doyley <jdoyley@gmail.com>
2017-10-19 09:01:58 +0100

    First commit

---
+++ text_client/lib/text_client/interact.ex
@@ -0,0 +1,19 @@
+defmodule TextClient.Interact do
+
+  alias TextClient.State
+
+  def start() do
+    Hangman.new_game()
+    |> setup_state()
+  end
+
+  defp setup_state(game) do
+    %State{
+      game_service: game,
+      tally: Hangman.tally(game),
+    }
+  end
+
+  def play(state) do
+  end
+end
