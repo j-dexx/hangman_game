109e329f7f522c7b275d9a2da86a48b22d8cdffa
James Doyley <jdoyley@gmail.com>
2017-10-19 13:40:24 +0100

    Add letters guessed to output

--- text_client/lib/text_client/summary.ex
+++ text_client/lib/text_client/summary.ex
@@ -4,6 +4,7 @@ defmodule TextClient.Summary do
       "\n",
       "Word so far: #{Enum.join(tally.letters, " ")}\n",
       "Guesses left: #{tally.turns_left}\n",
+      "Letters guessed: #{Enum.join(tally.letters_guessed, " ")}\n",
     ]
     game
   end
