c74fcf9afe5631ed5fa518b86efda254a79f9290
James Doyley <jdoyley@gmail.com>
2017-10-19 16:33:22 +0100

    Refactor dictionary api

--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -12,7 +12,9 @@ defmodule Hangman.Game do
     }
   end
   def new_game() do
-    new_game(Dictionary.random_word)
+    Dictionary.start()
+    |> Dictionary.random_word()
+    |> new_game()
   end
 
   def make_move(game = %{ game_state: state }, _guess) when state in [:won, :lost] do
@@ -66,7 +68,7 @@ defmodule Hangman.Game do
   end
 
   defp reveal_letter(letter, _in_word = true), do: letter
-  defp reveal_letter(letter, _not_in_word), do: "_"
+  defp reveal_letter(_letter, _not_in_word), do: "_"
 
   defp maybe_won(true), do: :won
   defp maybe_won(_), do: :good_guess
