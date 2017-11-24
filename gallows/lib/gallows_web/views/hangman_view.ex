cefc7809d07c6720f68c2f34ce8a32f279dca7e7
James Doyley <jdoyley@gmail.com>
2017-11-24 09:00:54 +0000

    Update gallows
    - Allowing start of new game
    - Outputting game state
    - Outputting letters guessed

--- gallows/lib/gallows_web/views/hangman_view.ex
+++ gallows/lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,20 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+  import GallowsWeb.Views.Helpers.GameStateHelper
 
-  def word_so_far(tally) do
-    tally.letters |> Enum.join(" ")
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game))
+  end
+
+  def letters_guessed(%{ letters_guessed: letters_guessed }) do
+    letters_guessed |> Enum.join(" ")
+  end
+
+  def word_so_far(%{ letters: letters }) do
+    letters |> Enum.join(" ")
   end
 end
